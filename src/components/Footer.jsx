fed28cf7a421b50794f2bcb4dfa530a66f62cd4e
atish <kumaratishranjan2003@gmail.com>
2025-09-25 15:10:37 +0530

    update content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaCloud  } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
@@ -11,15 +12,15 @@ const Footer = () => {
           <div className="space-y-3">
             <Link to="/about_journal" className="block hover:underline">About Journal of Health Politics, Policy and Law</Link>
             <Link to="/editorial_board" className="block hover:underline">Editorial Board</Link>
-            <a href="#" className="block hover:underline">For Authors</a>
-            <a href="#" className="block hover:underline">Twitter</a>
+            <Link to="#" className="block hover:underline">For Authors</Link>
+            <Link to="https://x.com/JHPPL" className="block hover:underline">Twitter</Link>
           </div>
           {/* Center links */}
           <div className="space-y-3">
-            <a href="#" className="block hover:underline">Facebook</a>
-            <a href="#" className="block hover:underline">Purchase</a>
+            <Link to="https://www.facebook.com/JournalofHealthPoliticsPolicyandLaw" className="block hover:underline">Facebook</Link>
+            <Link to="#" className="block hover:underline">Purchase</Link>
             <Link to="/advertising" className="block hover:underline">Advertise</Link>
-            <a href="#" className="block hover:underline">Rights and Permissions Inquiry</a>
+            <Link to="/content" className="block hover:underline">Rights and Permissions Inquiry</Link>
           </div>
           {/* Journal title */}
           <div className="flex flex-col items-end justify-between h-full">
@@ -55,48 +56,82 @@ const Footer = () => {
             <div className="font-bold mb-2">Information For</div>
             <div className="space-y-1">
               <Link to="/advertising" className="block hover:underline">Advertisers</Link>
-              <a href="#" className="block hover:underline">Book Authors</a>
-              <a href="#" className="block hover:underline">Booksellers/Media</a>
-              <a href="#" className="block hover:underline">Customers</a>
-              <a href="#" className="block hover:underline">Educators</a>
-              <a href="#" className="block hover:underline">Journal Authors/Editors</a>
-              <a href="#" className="block hover:underline">Librarians</a>
-              <a href="#" className="block hover:underline">Prospective Journals</a>
-              <a href="#" className="block hover:underline">Licensing and Subsidiary Rights</a>
-              <a href="#" className="block hover:underline">Societies</a>
+              <Link to="/bookauthor" className="block hover:underline">Book Authors</Link>
+              <Link to="/content" className="block hover:underline">Booksellers/Media</Link>
+              <Link to="/content" className="block hover:underline">Customers</Link>
+              <Link to="/content" className="block hover:underline">Educators</Link>
+              <Link to="/bookauthor" className="block hover:underline">Journal Authors/Editors</Link>
+              <Link to="/library" className="block hover:underline">Librarians</Link>
+              <Link to="/pj" className="block hover:underline">Prospective Journals</Link>
+              <Link to="/license" className="block hover:underline">Licensing and Subsidiary Rights</Link>
+              <Link to="/society" className="block hover:underline">Societies</Link>
             </div>
           </div>
           {/* Careers & Mailing List */}
           <div>
             <div className="font-bold mb-2">Careers</div>
-            <a href="#" className="block mb-4 hover:underline">View Open Positions</a>
+            <a href="https://www.dukeupress.edu/Careers/" className="block mb-4 hover:underline">View Open Positions</a>
             <button className="bg-[#a07bb7] text-white px-6 py-2 rounded w-full flex items-center justify-center mb-4">
-              <span className="mr-2">📧</span> Join Our Mailing List
+              <span className="mr-2" href="https://signup.e2ma.net/signup/2001912/21407/">📧</span> Join Our Mailing List
             </button>
             <button className="bg-[#a07bb7] text-white px-6 py-2 rounded w-full flex items-center justify-center">
-              <span className="mr-2">📄</span> Current Catalog
+              <span className="mr-2" href="https://www.dukeupress.edu/Catalog/">📄</span> Current Catalog
             </button>
           </div>
           {/* Connect & Logo */}
           <div>
             <div className="font-bold mb-2">Connect</div>
-            <div className="flex space-x-2 mb-4">
-              <a href="#" className="bg-black border border-white rounded p-2 flex items-center justify-center">
-                <span className="text-xl">📘</span>
-              </a>
-              <a href="#" className="bg-black border border-white rounded p-2 flex items-center justify-center">
-                <span className="text-xl">🦋</span>
-              </a>
-              <a href="#" className="bg-black border border-white rounded p-2 flex items-center justify-center">
-                <span className="text-xl">✖️</span>
-              </a>
-              <a href="#" className="bg-black border border-white rounded p-2 flex items-center justify-center">
-                <span className="text-xl">📺</span>
-              </a>
-              <a href="#" className="bg-black border border-white rounded p-2 flex items-center justify-center">
-                <span className="text-xl">📷</span>
-              </a>
-            </div>
+           <div className="flex space-x-2 mb-4">
+      {/* Facebook */}
+      <a
+        href="https://www.facebook.com/DukeUniversityPress"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-black border border-white rounded p-2 flex items-center justify-center"
+      >
+        <FaFacebook className="text-xl text-white" />
+      </a>
+
+      {/* Bluesky */}
+      <a
+        href="https://bsky.app/profile/dukepress.bsky.social"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-black border border-white rounded p-2 flex items-center justify-center"
+      >
+        <FaCloud  className="text-xl text-white" />
+      </a>
+
+      {/* Twitter (X) */}
+      <a
+        href="https://x.com/DUKEpress"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-black border border-white rounded p-2 flex items-center justify-center"
+      >
+        <FaTwitter className="text-xl text-white" />
+      </a>
+
+      {/* YouTube */}
+      <a
+        href="https://www.youtube.com/user/DukeUPressPublicity"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-black border border-white rounded p-2 flex items-center justify-center"
+      >
+        <FaYoutube className="text-xl text-white" />
+      </a>
+
+      {/* Instagram */}
+      <a
+        href="https://www.instagram.com/dukeuniversitypress/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-black border border-white rounded p-2 flex items-center justify-center"
+      >
+        <FaInstagram className="text-xl text-white" />
+      </a>
+    </div>
           </div>
         </div>
         <div className="max-w-7xl mx-auto mt-8 text-xs text-gray-300 flex flex-wrap justify-between items-center">
